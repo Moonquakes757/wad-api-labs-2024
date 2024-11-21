66f4628bd7cb6de32baff58f3c1e207efe31ca91
Moonquakes757 <xsh20020902@gmail.com>
2024-11-21 15:17:42 +0000

    Express Lab - exercise done

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -21,13 +21,16 @@ router.get('/:id', (req, res) => {
 //Add a task
 router.post('/', (req, res) => {
     const { title, description, deadline, priority, done } = req.body;
+    const now = new Date().toISOString();
     const newTask = {
         id: uuidv4(),
         title,
         description,
         deadline,
         priority,
-        done
+        done,
+        created_at: now,
+        updated_at: now
     };
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
@@ -41,7 +44,13 @@ router.put('/:id', (req, res) => {
     if (taskIndex === -1) {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
-    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    const now = new Date().toISOString();
+    const updatedTask = { 
+        ...tasksData.tasks[taskIndex], 
+        ...req.body,
+        id:id,
+        updated_at: now
+    };
     tasksData.tasks[taskIndex] = updatedTask;
     res.json(updatedTask);
 });
